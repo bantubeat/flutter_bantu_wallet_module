97b3c8561580e7cf27940a1e607876d8dc38302b
unknown <meztsacar@gmail.com>
2025-06-03 14:45:39 +0100

    Fix bg color of bzc page

--- lib/src/layers/presentation/pages/beatzcoins/beatzcoins_page.dart
+++ lib/src/layers/presentation/pages/beatzcoins/beatzcoins_page.dart
@@ -68,7 +68,7 @@ class BeatzcoinsPage extends StatelessWidget {
               const SizedBox(height: 2),
               Container(
                 padding: const EdgeInsets.all(5),
-                color: colorScheme.primaryContainer.withValues(alpha: 0.5),
+                color: colorScheme.primary.withValues(alpha: 0.2),
                 alignment: Alignment.center,
                 child: RichText(
                   textAlign: TextAlign.justify,
